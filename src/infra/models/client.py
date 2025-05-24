7e84adaab752ab46c38ecdcb928b4869e4ae9d26
isaac-Foster <isack200961@hotmail.com>
2025-05-24 13:26:51 -0300

    feat: add new models  and event in products

---
+++ src/infra/models/client.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import mapped_column, Mapped
+
+from src.infra.database.sql import reg
+
+
+@reg.mapped_as_dataclass
+class ClientModel:
+    __tablename__ = 'clients'
+
+    id: Mapped[int] = mapped_column(Integer, init=False, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(60))
+    document: Mapped[str] = mapped_column(String(11), unique=True, index=True)
--- src/infra/models/product.py
+++ src/infra/models/product.py
@@ -1,6 +1,18 @@
 from decimal import Decimal
 from typing import List, Optional
-from sqlalchemy import String, Integer, Numeric, ARRAY, Date
+from datetime import datetime
+
+
+from sqlalchemy import (
+    String,
+    Integer,
+    Numeric,
+    ARRAY,
+    DateTime,
+    event,
+    Boolean,
+)
+
 from sqlalchemy.orm import mapped_column, Mapped
 from src.infra.database.sql import reg
 
@@ -9,15 +21,26 @@ from src.infra.database.sql import reg
 class ProductModel:
     __tablename__ = 'products'
 
-    id: Mapped[int] = mapped_column(Integer, init=False, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(
+        Integer, init=False, primary_key=True, autoincrement=True
+    )
     name: Mapped[str] = mapped_column(String(255), index=True)
     price: Mapped[Decimal] = mapped_column(Numeric(19, 4))
     description: Mapped[str] = mapped_column(String(512), index=True)
     barcode: Mapped[str] = mapped_column(String(60), unique=True, index=True)
-    section: Mapped[str] = mapped_column(String(50))
+    section: Mapped[str] = mapped_column(String(50), index=True)
     stock: Mapped[int] = mapped_column(Integer)
-    expiration_date: Mapped[Optional[str]] = mapped_column(Date, nullable=True)
+    expiration_date: Mapped[Optional[datetime]] = mapped_column(
+        DateTime, nullable=True
+    )
+    disponibility: Mapped[bool] = mapped_column(Boolean, default=True)
     images: Mapped[List[str]] = mapped_column(
         ARRAY(String),
         default=list,
     )
+
+
+@event.listens_for(ProductModel, 'before_insert')
+@event.listens_for(ProductModel, 'before_update')
+def update_disponibility(mapper, connection, target: ProductModel):
+    target.disponibility = target.stock > 0
