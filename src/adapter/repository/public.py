66b589305bd0e830f0934a82d71036fdf9f6108e
isaac-Foster <isack200961@hotmail.com>
2025-05-24 13:20:37 -0300

    chore: add more validations

--- src/adapter/repository/public.py
+++ src/adapter/repository/public.py
@@ -9,7 +9,6 @@ class BaseRepo(IUBaseRepoPort):
     def __init__(self):
         self.session = Session
         self.model = None
-    
 
     def find(self, **kwargs):
         with self.session() as session:
@@ -22,8 +21,8 @@ class BaseRepo(IUBaseRepoPort):
         page: int = None,
         limit: int = None,
         period: dict = None,
-        **filters
-        ):
+        **filters,
+        ): #noqa: F821
         with self.session() as session:
             stmt = select(self.model)
 
@@ -32,20 +31,26 @@ class BaseRepo(IUBaseRepoPort):
             for key, value in filters.items():
                 if value is None or not hasattr(self.model, key):
                     continue
-                
-                if key in {"name", "email"}:
-                    conditions.append(getattr(self.model, key).ilike(f"%{value}%"))
+
+                if key in {'name', 'email'}:
+                    conditions.append(
+                        getattr(self.model, key).ilike(f'%{value}%')
+                    )
                 else:
                     conditions.append(getattr(self.model, key) == value)
 
             if period and isinstance(period, dict):
-                start = period.get("start")
-                end = period.get("end")
-                if hasattr(self.model, "created_at"):
+                start = period.get('start')
+                end = period.get('end')
+                if hasattr(self.model, 'created_at'):
                     if start:
-                        conditions.append(getattr(self.model, "created_at") >= start)
+                        conditions.append(
+                            getattr(self.model, 'created_at') >= start
+                        )
                     if end:
-                        conditions.append(getattr(self.model, "created_at") <= end)
+                        conditions.append(
+                            getattr(self.model, 'created_at') <= end
+                        )
 
             if conditions:
                 stmt = stmt.where(and_(*conditions))
@@ -55,15 +60,18 @@ class BaseRepo(IUBaseRepoPort):
                 stmt = stmt.offset(offset).limit(limit)
 
             return session.execute(stmt).scalars().all()
-    
+
     def delete(self, **kwargs):
+        already = self.find(**kwargs)
+
+        if not already:
+            raise Exception('user not found')
+
         with self.session() as session:
             try:
-                session.execute(
-                    delete(self.model).filter_by(**kwargs)
-                )
+                session.execute(delete(self.model).filter_by(**kwargs))
                 session.commit()
                 return True
-            except:
+            except Exception as e:
+                print(e)
                 return False
-
