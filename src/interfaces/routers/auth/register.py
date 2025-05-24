48f118fa1da75bd024a77c2457f08ba6f0125ada
isaac-Foster <isack200961@hotmail.com>
2025-05-24 13:21:35 -0300

    chore: add responses exemples

--- src/interfaces/routers/auth/register.py
+++ src/interfaces/routers/auth/register.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter
- 
+from fastapi import APIRouter, Depends
+
+from src.adapter.controllers.user import UserController
+from src.interfaces.schema.auth import UserRegisterSchema, responses_register
+
 router = APIRouter(
     prefix='/auth',
     tags=[
         'auth',
-    ]
+    ],
 )
 
-@router.post('/register')
-async def register():
-    return {}
+
+@router.post('/register', responses=responses_register)
+async def register(
+    data: UserRegisterSchema,
+    controller: UserController = Depends(UserController),
+):
+    data = controller.create(data)
+    return data
